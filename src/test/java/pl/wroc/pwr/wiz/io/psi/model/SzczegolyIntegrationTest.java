6a7384932c664867bdab7700b5cef16ec824be6a
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 14:11:30 +0100

    added Szczegoly model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/SzczegolyIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Szczegoly.class)
+public class SzczegolyIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
