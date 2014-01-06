26c93ba645daab1d56c75b2d019c67d664f7c585
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 13:13:51 +0100

    added Oferta model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/OfertaIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Oferta.class)
+public class OfertaIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
