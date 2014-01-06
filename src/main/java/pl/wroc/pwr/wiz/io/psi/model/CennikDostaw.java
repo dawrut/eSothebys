6dd2d5e5f4e1df969b797b5e115001b0076e60ad
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 12:51:16 +0100

    added CennikDostaw model class, to be continued

---
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/model/CennikDostaw.java
@@ -0,0 +1,20 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.beans.factory.annotation.Value;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class CennikDostaw {
+
+    /**
+     */
+    private String nazwa;
+
+    /**
+     */
+    @Value("false")
+    private Boolean szablon;
+}
