f736a826798aded52e8109a8a4b84bcb4c68aab9
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 13:58:59 +0100

    added Wojewodztwo model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/WojewodztwoIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Wojewodztwo.class)
+public class WojewodztwoIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
