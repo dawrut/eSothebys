b576f2bd1f544c3ec5da8c376534b2ffc9a9cf2f
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 14:18:32 +0100

    added SubskrypcjaUlubionejKategorii model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/SubskrypcjaUlubionejKategoriiIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = SubskrypcjaUlubionejKategorii.class)
+public class SubskrypcjaUlubionejKategoriiIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
