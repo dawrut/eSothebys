b9c86560ffc010588c2f5038eb5e53e456aee9f1
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 12:17:09 +0100

    added Adres model class, to be continued

---
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/model/Adres.java
@@ -0,0 +1,22 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Adres {
+
+    /**
+     */
+    private String kodPocztowy;
+
+    /**
+     */
+    private String miejscowosc;
+
+    /**
+     */
+    private String ulicaZNumerem;
+}
---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/AdresIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Adres.class)
+public class AdresIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
