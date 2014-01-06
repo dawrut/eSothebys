dd2d503e28c73e46bd40e3a5ba2aacbf6d375d7d
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 13:45:11 +0100

    added WniosekOUniewaznienie model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/WniosekOUniewaznienieIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = WniosekOUniewaznienie.class)
+public class WniosekOUniewaznienieIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
