a72cb0265cab620b9b784f6c07cf7ca5f9c33cbe
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 14:44:41 +0100

    added KontoFakturowe model class, to be continued

---
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/model/KontoFakturowe.java
@@ -0,0 +1,20 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class KontoFakturowe {
+
+    /**
+     */
+    @NotNull
+    private String nip;
+
+    /**
+     */
+    private String nazwaDoFaktury;
+}
---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/KontoFakturoweIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = KontoFakturowe.class)
+public class KontoFakturoweIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
