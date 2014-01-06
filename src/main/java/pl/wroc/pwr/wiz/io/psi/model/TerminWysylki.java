c763078b9dcb3d7e62fe1a0580d1afe40e2ea997
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 12:26:18 +0100

    added TerminWysylki enum

---
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/model/TerminWysylki.java
@@ -0,0 +1,6 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+
+public enum TerminWysylki {
+
+    NATYCHMIAST, DZIEN, DWA_DNI, TRZY_DNI, CZTERY_DNI, PIEC_DNI, TYDZIEN, DZIESIEC_DNI, DWA_TYGODNIE, TRZY_TYGODNIE
+}
