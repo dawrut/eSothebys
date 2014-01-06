35ebc350865457f78cfac3a7f95a5ff0be2d0765
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 12:56:28 +0100

    added SzczegolyDostawy model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/SzczegolyDostawyIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = SzczegolyDostawy.class)
+public class SzczegolyDostawyIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
