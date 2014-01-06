ee15e6be006b12e976ed0130109fc50cdf39d9d1
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 13:51:33 +0100

    added GraficznaOcenaSprzedazy model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/GraficznaOcenaSprzedazyIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = GraficznaOcenaSprzedazy.class)
+public class GraficznaOcenaSprzedazyIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
