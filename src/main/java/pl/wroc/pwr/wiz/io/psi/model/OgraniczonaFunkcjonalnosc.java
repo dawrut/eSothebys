fb9cd40f9b8e87a63402d5061c46c691ec4e25a2
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 12:11:28 +0100

    added OgraniczonaFunkcjonalnosc model class, to be continued

---
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/model/OgraniczonaFunkcjonalnosc.java
@@ -0,0 +1,20 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import java.util.Date;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class OgraniczonaFunkcjonalnosc {
+
+    /**
+     */
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(style = "M-")
+    private Date odKiedy;
+}
---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/OgraniczonaFunkcjonalnoscDataOnDemand.java
@@ -0,0 +1,6 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = OgraniczonaFunkcjonalnosc.class)
+public class OgraniczonaFunkcjonalnoscDataOnDemand {
+}
