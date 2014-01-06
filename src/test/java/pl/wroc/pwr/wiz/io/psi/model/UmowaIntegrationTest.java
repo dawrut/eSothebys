dd65269c715f7f9e62ed219bb0989698e6de953b
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-01-06 13:12:00 +0100

    added Umowa model class, to be continued

---
+++ src/test/java/pl/wroc/pwr/wiz/io/psi/model/UmowaIntegrationTest.java
@@ -0,0 +1,11 @@
+package pl.wroc.pwr.wiz.io.psi.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Umowa.class)
+public class UmowaIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
