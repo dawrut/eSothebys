9879949620510daafabd047e1a74dd9ac087afa2
Dawid Rutowicz <dawid.rutowicz@gmail.com>
2014-02-01 22:01:22 +0100

    added custom authentication implementation

--- src/main/java/pl/wroc/pwr/wiz/io/psi/model/security/RolaUzytkownik.java
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/model/security/RolaUzytkownik.java
@@ -1,34 +1,31 @@
 package pl.wroc.pwr.wiz.io.psi.model.security;
-
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
-
 import pl.wroc.pwr.wiz.io.psi.model.Uzytkownik;
 
 @RooJavaBean
 @RooToString
-@RooJpaActiveRecord
 @Table(name = "rola_uzytkownik")
+@RooJpaActiveRecord(finders = { "findRolaUzytkowniksByEmail" })
 public class RolaUzytkownik {
 
-  /**
+    /**
      */
-  @ManyToOne
-  @JoinColumn(name = "email")
-  private Uzytkownik email;
+    @ManyToOne
+    @JoinColumn(name = "email")
+    private Uzytkownik email;
 
-  /**
+    /**
      */
-  @ManyToOne
-  @JoinColumn(name = "id_roli")
-  private Rola idRoli;
-  
-  public String getNazwisko() {
-    return email.getNazwisko();
-  }
+    @ManyToOne
+    @JoinColumn(name = "id_roli")
+    private Rola idRoli;
+
+    public String getNazwisko() {
+        return email.getNazwisko();
+    }
 }
---
+++ src/main/java/pl/wroc/pwr/wiz/io/psi/service/security/AuthenticationProviderImpl.java
@@ -0,0 +1,58 @@
+package pl.wroc.pwr.wiz.io.psi.service.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import pl.wroc.pwr.wiz.io.psi.model.Uzytkownik;
+import pl.wroc.pwr.wiz.io.psi.model.security.RolaUzytkownik;
+
+@Component
+public class AuthenticationProviderImpl implements AuthenticationProvider {
+
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String username = authentication.getName();
+    String password = (String) authentication.getCredentials();
+
+    Uzytkownik uzytkownik = Uzytkownik.findUzytkowniksByEmailEquals(username).getSingleResult();
+
+    String password21 = uzytkownik.getHaslo();
+
+    if (uzytkownik == null) {
+      // TODO: dodac i18n
+      throw new BadCredentialsException("Błędny użytkownik lub hasło.");
+    }
+
+    if (!password.equals(uzytkownik.getHaslo())) {
+      // TODO: dodac i18n
+      throw new BadCredentialsException("Błędny użytkownik lub hasło.");
+    }
+
+    Collection<GrantedAuthority> authorities = getUzytkownikAuthorities(uzytkownik);
+
+    return new UsernamePasswordAuthenticationToken(username, password, authorities);
+  }
+
+  private Collection<GrantedAuthority> getUzytkownikAuthorities(Uzytkownik uzytkownik) {
+    Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+    for (RolaUzytkownik rola : RolaUzytkownik.findRolaUzytkowniksByEmail(uzytkownik)
+        .getResultList()) {
+      authorities.add(new SimpleGrantedAuthority(rola.getIdRoli().getNazwaRoli()));
+    }
+    return authorities;
+  }
+
+  public boolean supports(Class<? extends Object> authentication) {
+    return true;
+  }
+
+}
